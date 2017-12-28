eec2e411311bd5f46aca1f9ff7fee2969119690d
phantomics <a@imagegardenphoto.com>
2017-12-28 08:48:25 -0700

    function fixes and new spec format

--- package.lisp
+++ package.lisp
@@ -2,6 +2,6 @@
 
 (defpackage #:apex
   (:export #:apex)
-  (:use #:cl #:alexandria #:array-operations #:maxpc 
-        #:cl-slice #:cl-ppcre #:parse-number #:symbol-munger)
+  (:use #:cl #:alexandria #:array-operations #:maxpc #:cl-slice
+	#:cl-ppcre #:parse-number #:symbol-munger #:prove)
   (:shadowing-import-from #:array-operations #:split #:flatten))
