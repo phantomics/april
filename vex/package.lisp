fa6c366038e2433e13c5fabbddb3bd5b6c520d70
phantomics <a@imagegardenphoto.com>
2017-12-31 12:08:37 -0700

    evaluate function fix

--- vex/package.lisp
+++ vex/package.lisp
@@ -1,8 +1,8 @@
 ;;;; package.lisp
 
 (defpackage #:vex
-  (:export #:vex-spec #:ambivalent #:monadic #:dyadic #:boolean-op #:args
-	   #:of-environment)
+  (:export #:vex-spec #:vex-program #:ambivalent #:monadic #:dyadic
+	   #:boolean-op #:args #:of-environment)
   (:use #:cl #:alexandria #:array-operations #:maxpc #:cl-ppcre #:symbol-munger #:prove)
   (:shadowing-import-from #:array-operations #:split #:flatten))
 
