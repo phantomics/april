de5e910160d59fe838ef87e72bf3c003147d783f
phantomics <a@imagegardenphoto.com>
2018-01-09 00:14:19 -0800

    grade functions complete

--- aplesque/package.lisp
+++ aplesque/package.lisp
@@ -3,7 +3,7 @@
 (defpackage #:aplesque
   (:export #:array-match #:array-depth #:multidim-slice #:scan-back #:make-back-scanner
 	   #:apply-marginal #:expand-array #:enlist #:array-inner-product #:index-of
-	   #:grade #:alpha-compare #:find-array #:run-dim #:invert-matrix
+	   #:grade #:array-grade #:alpha-compare #:find-array #:run-dim #:invert-matrix
 	   #:reshape-array-fitting #:sprfact #:binomial #:make-rotator
 	   #:rotate-left #:rotate-right)
   (:use #:cl #:alexandria #:array-operations #:parse-number #:symbol-munger)
