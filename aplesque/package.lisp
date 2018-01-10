573f6a5a146a9ef99aa5fdb0e63f798c0ca5a09b
phantomics <a@imagegardenphoto.com>
2018-01-09 22:45:14 -0800

    more functionality and tests

--- aplesque/package.lisp
+++ aplesque/package.lisp
@@ -1,10 +1,10 @@
 ;;;; package.lisp
 
 (defpackage #:aplesque
-  (:export #:array-match #:array-depth #:multidim-slice #:scan-back #:make-back-scanner
-	   #:apply-marginal #:expand-array #:enlist #:array-inner-product #:index-of
+  (:export #:array-promote #:array-match #:array-depth #:multidim-slice #:scan-back #:make-back-scanner
+	   #:array-to-list #:apply-marginal #:expand-array #:enlist #:array-inner-product #:index-of
 	   #:grade #:array-grade #:alpha-compare #:find-array #:run-dim #:invert-matrix
-	   #:reshape-array-fitting #:sprfact #:binomial #:make-rotator
+	   #:reshape-array-fitting #:sprfact #:binomial #:make-rotator #:mix-arrays
 	   #:rotate-left #:rotate-right)
   (:use #:cl #:alexandria #:array-operations #:parse-number #:symbol-munger)
   (:shadowing-import-from #:array-operations #:flatten))
