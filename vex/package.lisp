b1794afc63a1a104d4d89d7e7e89def5ae4d986a
phantomics <a@imagegardenphoto.com>
2018-01-16 14:43:01 -0800

    refactoring

--- vex/package.lisp
+++ vex/package.lisp
@@ -2,6 +2,6 @@
 
 (defpackage #:vex
   (:export #:vex-spec #:vex-program #:ambivalent #:monadic #:dyadic
-	   #:boolean-op #:args #:of-state)
+	   #:boolean-op #:args #:of-state #:of-functions #:of-operators #:of-overloaded?)
   (:use #:cl #:alexandria #:maxpc #:cl-ppcre #:symbol-munger #:prove))
 
