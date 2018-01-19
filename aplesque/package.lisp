75fb9a27e67116b6065efb44c31d355cd18d3a57
phantomics <a@imagegardenphoto.com>
2018-01-19 14:05:56 -0800

    non-square matrix inverse working

--- aplesque/package.lisp
+++ aplesque/package.lisp
@@ -3,9 +3,9 @@
 (defpackage #:aplesque
   (:export #:array-promote #:array-match #:array-depth #:multidim-slice #:scan-back #:make-back-scanner
 	   #:array-to-list #:apply-marginal #:expand-array #:enlist #:array-inner-product #:index-of
-	   #:grade #:array-grade #:alpha-compare #:array-compare #:find-array #:run-dim #:invert-matrix
+	   #:grade #:array-grade #:alpha-compare #:array-compare #:find-array #:run-dim
 	   #:reshape-array-fitting #:sprfact #:binomial #:make-rotator #:scale-array #:mix-arrays
-	   #:rotate-left #:rotate-right)
+	   #:rotate-left #:rotate-right #:invert-matrix)
   (:use #:cl #:alexandria #:array-operations #:parse-number #:symbol-munger)
   (:shadowing-import-from #:array-operations #:flatten))
 
