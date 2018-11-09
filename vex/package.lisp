f6fffac7698b48f26e77fd391932ba18c35d698d
phantomics <a@imagegardenphoto.com>
2018-11-08 18:37:52 -0800

    @ mostly implemented, workspace improvements

--- vex/package.lisp
+++ vex/package.lisp
@@ -1,7 +1,7 @@
 ;;;; package.lisp
 
 (defpackage #:vex
-  (:export #:vex-spec #:vex-program #:ambivalent #:monadic #:dyadic #:reverse-op #:boolean-op
+  (:export #:local-idiom #:vex-spec #:vex-program #:ambivalent #:monadic #:dyadic #:reverse-op #:boolean-op
 	   #:reverse-boolean-op #:args #:of-state #:of-functions #:of-operators #:of-overloaded?)
   (:use #:cl #:alexandria #:maxpc #:cl-ppcre #:symbol-munger #:prove))
 
