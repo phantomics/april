e854022290e117800cbedb914441910f9872486a
phantomics <a@imagegardenphoto.com>
2018-11-10 17:37:17 -0800

    @ operator mostly done, rank operator started

--- aplesque/package.lisp
+++ aplesque/package.lisp
@@ -3,7 +3,7 @@
 (defpackage #:aplesque
   (:export #:array-promote #:array-match #:array-depth #:multidim-slice #:scan-back #:make-back-scanner
 	   #:array-to-list #:apply-marginal #:expand-array #:enlist #:array-inner-product #:index-of
-	   #:grade #:array-grade #:alpha-compare #:array-compare #:find-array #:ravel #:run-dim
+	   #:grade #:array-grade #:alpha-compare #:array-compare #:find-array #:ravel #:run-dim #:disclose
 	   #:reshape-array-fitting #:sprfact #:binomial #:make-rotator #:scale-array #:mix-arrays
 	   #:aref-eliding #:rotate-left #:rotate-right #:partitioned-enclose #:re-enclose #:invert-matrix)
   (:use #:cl #:alexandria #:array-operations #:parse-number #:symbol-munger)
