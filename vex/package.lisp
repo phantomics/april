e887afdc1f6a85e8d435374192ca23e9fd371321
phantomics <a@imagegardenphoto.com>
2018-12-19 00:59:19 -0800

    fixes to ○, compilation and more

--- vex/package.lisp
+++ vex/package.lisp
@@ -2,7 +2,7 @@
 
 (defpackage #:vex
   (:export #:local-idiom #:vex-spec #:vex-program #:ambivalent #:monadic #:dyadic #:reverse-op #:boolean-op
-	   #:reverse-boolean-op #:args #:of-state #:of-functions #:of-operators #:of-overloaded?
+	   #:reverse-boolean-op #:args #:of-functions #:of-operators #:of-overloaded?
 	   #:composer #:set-composer-elements #:set-composer-patterns #:of-system)
   (:use #:cl #:alexandria #:maxpc #:cl-ppcre #:symbol-munger #:prove))
 
