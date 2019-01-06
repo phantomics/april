ad0b0f876adbc345e8061ccdb97eb60195e401c9
phantomics <a@imagegardenphoto.com>
2019-01-06 10:25:39 -0800

    printing improvements

--- aplesque/package.lisp
+++ aplesque/package.lisp
@@ -1,11 +1,11 @@
 ;;;; package.lisp
 
 (defpackage #:aplesque
-  (:export #:array-promote #:array-match #:array-depth #:multidim-slice #:scan-back #:make-back-scanner
+  (:export #:array-promote #:array-match #:array-depth #:section #:scan-back #:make-back-scanner
 	   #:array-to-list #:apply-marginal #:expand-array #:enlist #:array-inner-product #:index-of
 	   #:grade #:array-grade #:alpha-compare #:array-compare #:find-array #:ravel #:across #:disclose
 	   #:reshape-array-fitting #:sprfact #:binomial #:make-rotator #:scale-array #:mix-arrays #:split-array
-	   #:is-unitary #:choose #:subprocess #:rotate-left #:rotate-right #:catenate #:laminate
+	   #:is-unitary #:choose #:subprocess #:rotate-left #:rotate-right #:catenate #:laminate #:enclose
 	   #:partitioned-enclose #:re-enclose #:invert-matrix #:interval-index #:disclose-unitary-array
 	   #:merge-arrays #:stencil #:matrix-render #:matrix-print)
   (:use #:cl #:alexandria #:array-operations #:parse-number #:symbol-munger)
