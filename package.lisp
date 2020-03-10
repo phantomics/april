6b20d4d64cbd4f37f5b03d2d1c3c0a56cc2798ac
phantomics <a@imagegardenphoto.com>
2020-03-09 17:37:44 -0700

    fix to ≠ for character comparison

--- package.lisp
+++ package.lisp
@@ -2,7 +2,7 @@
 ;;;; package.lisp
 
 (defpackage #:april
-  (:export #:april #:april-p #:april-c #:april-load #:with-april-context #:array-to-nested-vector)
+  (:export #:april #:april-f #:april-p #:april-c #:april-load #:with-april-context #:array-to-nested-vector)
   (:use #:cl #:aplesque #:vex #:alexandria #:array-operations #:decimals
 	#:cl-ppcre #:parse-number #:symbol-munger #:prove #:simple-date-time)
   (:shadowing-import-from #:array-operations #:split #:flatten))
