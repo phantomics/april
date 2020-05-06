14030691c879d8353207e98651d439395607d898
phantomics <a@imagegardenphoto.com>
2020-05-06 03:33:45 -0700

    varef function added to aplesque

--- aplesque/package.lisp
+++ aplesque/package.lisp
@@ -2,7 +2,7 @@
 ;;;; package.lisp
 
 (defpackage #:aplesque
-  (:export #:array-promote #:array-match #:array-depth #:section #:array-to-list #:apply-marginal
+  (:export #:varef #:array-promote #:array-match #:array-depth #:section #:array-to-list #:apply-marginal
 	   #:expand-array #:enlist #:array-inner-product #:index-of #:grade #:array-grade
 	   #:alpha-compare #:array-compare #:find-array #:ravel #:across #:disclose #:re-enclose
 	   #:reshape-to-fit #:sprfact #:binomial #:scale-array #:mix-arrays #:array-inner-product
