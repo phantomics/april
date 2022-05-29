ba99a13d3bcf5dc25072ca7741c9764dcbe75e4d
phantomics <a@imagegardenphoto.com>
2022-05-28 22:51:16 -0700

    lazify ∩∪

--- varray/package.lisp
+++ varray/package.lisp
@@ -6,11 +6,11 @@
            #:vvector-integer-progression #:vader-operate #:vader-select
            #:vader-shape #:vader-reshape #:vader-pare #:vader-catenate
            #:vader-mix #:vader-split #:vader-section #:vader-enclose #:vader-partition
-           #:vader-turn #:vader-permute #:vader-expand
-           #:vader-grade #:vader-encode #:vader-decode)
+           #:vader-intersection #:vader-unique #:vader-union #:vader-turn #:vader-permute
+           #:vader-expand #:vader-grade #:vader-encode #:vader-decode)
   (:shadowing-import-from #:aplesque #:enclose #:disclose #:disclose-unitary #:get-dimensional-factors
                           #:assign-element-type #:type-in-common #:apl-array-prototype
-                          #:apply-scalar #:is-unitary #:enclose-atom
+                          #:apply-scalar #:is-unitary #:enclose-atom #:array-compare
                           #:index-of #:alpha-compare #:grade #:array-grade)
   (:shadowing-import-from #:aplesque.forms #:indexer-split #:indexer-section
                           #:indexer-expand #:indexer-turn #:indexer-permute))
